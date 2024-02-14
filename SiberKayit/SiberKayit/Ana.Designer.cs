d49e11b77b228d2c345467b18430235c14defc19
Mehmet Eren Kaşıkçı <mehmeterenkasikci@gmail.com>
2024-02-14 14:00:20 +0300

    Design
    Training

--- SiberKayit/SiberKayit/Ana.Designer.cs
+++ SiberKayit/SiberKayit/Ana.Designer.cs
@@ -47,6 +47,7 @@ namespace SiberKayit
             this.btn_hepsinisil = new System.Windows.Forms.Button();
             this.lbl_saat = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -213,12 +214,23 @@ namespace SiberKayit
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(601, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 25);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Isparta Gençlik Merkezi";
+            // 
             // Ana
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(1429, 838);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_saat);
             this.Controls.Add(this.btn_hepsinisil);
             this.Controls.Add(this.btn_hepsinikaydet);
@@ -267,6 +279,7 @@ namespace SiberKayit
         private System.Windows.Forms.Button btn_hepsinisil;
         private System.Windows.Forms.Label lbl_saat;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 

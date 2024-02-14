4549ebc42431b4680d7858079d30ad3ddc0b72c8
astraxxx14 <xnnabsbsbsb@gmail.com>
2024-02-14 09:31:02 +0300

    Project
    Registration Automation

---
+++ SiberKayit/SiberKayit/Ana.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace SiberKayit
+{
+    partial class Ana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_tc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.txt_soyadi = new System.Windows.Forms.TextBox();
+            this.txt_adi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_arama = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tarihsaat = new System.Windows.Forms.Label();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_tc
+            // 
+            this.txt_tc.Location = new System.Drawing.Point(138, 222);
+            this.txt_tc.MaxLength = 11;
+            this.txt_tc.Name = "txt_tc";
+            this.txt_tc.Size = new System.Drawing.Size(164, 22);
+            this.txt_tc.TabIndex = 0;
+            this.txt_tc.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_tc_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tc No:";
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(34, 422);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.btn_ekle.TabIndex = 4;
+            this.btn_ekle.Text = "Kaydet";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_soyadi
+            // 
+            this.txt_soyadi.Location = new System.Drawing.Point(138, 325);
+            this.txt_soyadi.Name = "txt_soyadi";
+            this.txt_soyadi.Size = new System.Drawing.Size(164, 22);
+            this.txt_soyadi.TabIndex = 5;
+            // 
+            // txt_adi
+            // 
+            this.txt_adi.Location = new System.Drawing.Point(138, 279);
+            this.txt_adi.Name = "txt_adi";
+            this.txt_adi.Size = new System.Drawing.Size(164, 22);
+            this.txt_adi.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Soyad:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(380, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1103, 695);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick_1);
+            // 
+            // txt_arama
+            // 
+            this.txt_arama.Location = new System.Drawing.Point(798, 121);
+            this.txt_arama.MaxLength = 11;
+            this.txt_arama.Name = "txt_arama";
+            this.txt_arama.Size = new System.Drawing.Size(479, 22);
+            this.txt_arama.TabIndex = 12;
+            this.txt_arama.TextChanged += new System.EventHandler(this.txt_arama_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(710, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Kayıt Ara:";
+            // 
+            // tarihsaat
+            // 
+            this.tarihsaat.AutoSize = true;
+            this.tarihsaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tarihsaat.Location = new System.Drawing.Point(29, 18);
+            this.tarihsaat.Name = "tarihsaat";
+            this.tarihsaat.Size = new System.Drawing.Size(64, 25);
+            this.tarihsaat.TabIndex = 15;
+            this.tarihsaat.Text = "label6";
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(257, 422);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(85, 23);
+            this.btn_guncelle.TabIndex = 16;
+            this.btn_guncelle.Text = "Güncelle";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            this.btn_guncelle.Click += new System.EventHandler(this.btn_guncelle_Click);
+            // 
+            // Ana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1429, 838);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.tarihsaat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_arama);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_adi);
+            this.Controls.Add(this.txt_soyadi);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_tc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Ana";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_tc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.TextBox txt_soyadi;
+        private System.Windows.Forms.TextBox txt_adi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_arama;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label tarihsaat;
+        private System.Windows.Forms.Button btn_guncelle;
+    }
+}
+
---
+++ SiberKayit/SiberKayit/Ana.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiberKayit
+{
+    public partial class Ana : Form
+    {
+        SiberKayitEntities vt = new SiberKayitEntities();
+        private int kullaniciid;
+
+        public Ana()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tc = txt_tc.Text;
+            string adi = txt_adi.Text;
+            string soyadi = txt_soyadi.Text;
+            DateTime giris = DateTime.Now;
+
+
+
+            try
+            {
+
+                if (tc.Length != 11)
+                {
+                    MessageBox.Show("Geçerli Bir Tc Giriniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    islemler ekle = new islemler();
+                    ekle.kayit(adi, soyadi, tc, giris);
+                    temizle();
+                    listele();
+                }
+
+
+
+
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir Hata Meydana Geldi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                temizle();
+            }
+
+        }
+
+        public void temizle()
+        {
+            txt_adi.Text = string.Empty;
+            txt_soyadi.Text = string.Empty;
+            txt_tc.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            listele();
+            dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+
+            tarihsaat.Text = DateTime.Now.ToLongDateString();
+
+        }
+        public void listele()
+        {
+            dataGridView1.DataSource = vt.siber.ToList();
+            dataGridView1.Columns["id"].HeaderText = "Ziyaretçi No";
+            dataGridView1.Columns["tcNo"].HeaderText = "TC No";
+            dataGridView1.Columns["adi"].HeaderText = "Adı";
+            dataGridView1.Columns["soyadi"].HeaderText = "Soyadı";
+            dataGridView1.Columns["girisTarihi"].HeaderText = "Giriş Tarihi";
+            dataGridView1.Columns["cikisTarihi"].HeaderText = "Çıkış Tarihi";
+            dataGridView1.Columns["tcNo"].DisplayIndex = 3;
+            dataGridView1.Columns["id"].Width = 100;
+            dataGridView1.Columns["tcNo"].Width = 130;
+            dataGridView1.Columns["adi"].Width = 130;
+            dataGridView1.Columns["soyadi"].Width = 130;
+            dataGridView1.Columns["girisTarihi"].Width = 130;
+            dataGridView1.Columns["cikisTarihi"].Width = 130;
+        }
+
+
+        private void txt_tc_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                DialogResult sonuc = MessageBox.Show("Seçili Kaydı Silmek İstediğinizden Emin Misiniz?", "Kayıt Silme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (sonuc == DialogResult.Yes)
+                {
+                    try
+                    {
+                        DataGridViewRow row = dataGridView1.CurrentRow;
+                        int secilenid = (int)row.Cells["id"].Value;
+                        islemler sil = new islemler();
+                        sil.sil(secilenid);
+                        listele();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Bir Hata Meydana Geldi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Bir Satır Seçiniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            string columnName = dataGridView1.Columns[e.ColumnIndex].Name;
+
+            if (columnName == "cikisTarihi" && e.Value == null)
+            {
+                dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.Red;
+            }
+        }
+
+        private void dataGridView1_CellMouseDoubleClick_1(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                int secilenid = (int)row.Cells["id"].Value;
+                var secilenKayit = vt.siber.FirstOrDefault(x => x.id == secilenid);
+                if (secilenKayit != null)
+                {
+                    var sorgu = vt.siber.Where(x => x.id == secilenid && x.cikisTarihi != null).ToList();
+
+                    if (sorgu.Count > 0)
+                    {
+                        MessageBox.Show("Kişi Zaten Çıkış Yapmış!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    else
+                    {
+
+
+                        secilenKayit.cikisTarihi = DateTime.Now;
+                        vt.SaveChanges();
+                        listele();
+
+
+
+                    }
+
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txt_arama_TextChanged(object sender, EventArgs e)
+        {
+            string aramaKelimesi = txt_arama.Text.ToLower();
+
+            var filtrelenmisVeri = vt.siber.Where(x =>
+                x.TcNo.ToLower().Contains(aramaKelimesi) ||
+                x.adi.ToLower().Contains(aramaKelimesi) ||
+                x.Soyadi.ToLower().Contains(aramaKelimesi)
+            ).ToList();
+
+            dataGridView1.DataSource = filtrelenmisVeri;
+        }
+
+        private void btn_guncelle_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                DataGridViewRow row = dataGridView1.CurrentRow;
+                int secilenid = (int)row.Cells["id"].Value;
+                guncelle gunc = new guncelle(secilenid);
+                gunc.dataguncelleme += dataguncelle;
+                gunc.Show();
+            }
+
+
+        }
+
+        private void dataguncelle(object sender, EventArgs e)
+        {
+            listele();
+
+            int guncelid = ((guncelle)sender).kullaniciid;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (Convert.ToInt32(row.Cells["id"].Value) == guncelid)
+                {
+                    row.Cells["adi"].Value = ((guncelle)sender).txt_adi.Text;
+                    row.Cells["soyadi"].Value = ((guncelle)sender).txt_soyadi.Text;
+                    row.Cells["tcNo"].Value = ((guncelle)sender).txt_tc.Text;
+                    break;
+                }
+            }
+        }
+
+    }
+    
+ }
